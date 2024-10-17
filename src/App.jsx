685b79715c7b8b0694ec6638f9fc903087b03d13
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-17 21:13:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+function App() {
+  return (
+    <>
+      <div>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+        <button className="border-2 border-gray-400 m-1 leading-normal h-12 w-16">
+          X
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
